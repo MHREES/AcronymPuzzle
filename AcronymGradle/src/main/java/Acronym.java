436879b60e5b42d03a8bb77d39bce69ee8e1534b
Mathew Rees <mathew.rees@capgemini.com>
2017-06-06 10:47:41 +0100

    finishing touches

--- AcronymGradle/src/main/java/Acronym.java
+++ AcronymGradle/src/main/java/Acronym.java
@@ -3,18 +3,21 @@
 // Terminology ('Acronym'/'phrase') aligns with AcronymTest.java file 
 
 public class Acronym {
+	
+	// Create string variable to which the answer can be assigned... Made private so it's exclusive to this class
+    	private String answer;
 
-    	public String answer;
-
+	// Call the method MakeAcronym to create the acronym from an input 'phrase'
 	Acronym(String phrase){
 		answer = MakeAcronym(phrase);
 	}
-
-	String get(){
+	
+	// Create a method that returns answer as a string 'get()'. Made public so that it can be called from the test script without compromising 'answer'
+	public String get(){
 		return answer;
 	}
 
-	
+	// The method that executes the algorithm. 
 	public String MakeAcronym(String phrase){
         	// String PreProcessing: Add a space before uppercase letters (excludes acronymns). Changes the Char case pattern 'aBc' to 'a Bc'
         	String spacestoPhrase = phrase.replaceAll("[^A-Z]([A-Z][^AZ])", " $1");
